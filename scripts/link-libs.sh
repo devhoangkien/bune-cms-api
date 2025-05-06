ad9fb7906bbdc30cbdb177e6dfdd24531947e2cf
Hoàng Đức Kiên <46822388+devhoangkien@users.noreply.github.com>
2025-05-06 03:33:08 +0000

    update script install libs

--- scripts/link-libs.sh
+++ scripts/link-libs.sh
@@ -2,13 +2,8 @@
 
 set -e
 
-pushd "libs/casl-authorization/"
-bun i
-bun run build
-bun link
-popd
-pushd "libs/common/"
-bun i
-bun run build
-bun link
-popd
+# Build and link for "libs/casl-authorization/"
+(cd "libs/casl-authorization/" && bun i && bun run build && bun link)
+
+# Build and link for "libs/common/"
+(cd "libs/common/" && bun i && bun run build && bun link)
